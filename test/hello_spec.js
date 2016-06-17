6e6564be5837eb4c2f4dc59ab1ae414b3cb187e3
Kushagra <KR00446203@techmahindra.com>
2016-06-17 15:31:02 +0530

    test

--- test/hello_spec.js
+++ test/hello_spec.js
@@ -1,5 +1,5 @@
 describe("Hello", function() {
     it("says hello", function() {
-    expect(sayHello("kushagra")).toBe("Hello, kushagra how are you");
+    expect(sayHello("kushagra")).toBe("Hello, kushagra!");
     });
 });
